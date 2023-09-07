7f323ed7f1b1912eae14bf3ed23e4795622c0152
Miguel <miguellofredo@gmail.com>
2023-09-07 15:38:58 -0300

    Create class / activity ImcActivity

---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -0,0 +1,11 @@
+package co.tiagoaguiar.fitnesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -2,10 +2,17 @@ package co.tiagoaguiar.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var btnImc:LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+        btnImc.setOnClickListener {  }
     }
+
 }
