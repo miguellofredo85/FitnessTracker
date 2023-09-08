d6f11492ef0a6f02b1fe8033faef072988211c67
Miguel <miguellofredo@gmail.com>
2023-09-08 05:21:03 -0300

    Implementing logic of message from response calculate

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -6,6 +6,7 @@
     import android.widget.Button
     import android.widget.EditText
     import android.widget.Toast
+    import androidx.annotation.StringRes
 
     class ImcActivity : AppCompatActivity() {
         private lateinit var editHeight: EditText
@@ -28,7 +29,23 @@
                 val height = editHeight.text.toString().toInt()
                 val res = calcImc(weight, height)
                 Log.d("Teste", "resultado: $res") // log debug
+
+                val imcResponseMessage = imcResponse(res)
+                Toast.makeText(this, imcResponseMessage, Toast.LENGTH_LONG).show()
             }
+        }
+        private fun imcResponse(imc:Double):String{
+          return  when{
+              imc<15.0 -> return "1"
+              imc<16.0 -> return "2"
+              imc<18.5 -> return "3"
+              imc<25.0 -> return "4"
+              imc<30.0 -> return "5"
+              imc<35.0 -> return "6"
+              imc<40.0 -> return "7"
+              else -> "0"
+            }
+
         }
         private fun calcImc(weight: Int, height: Int):Double{
             return weight/((height/100.0)*(height/100.0))
