1aa73f64e43bf75c086def4dd4a4d5f6ca8289ba
Miguel <miguellofredo@gmail.com>
2023-09-09 06:17:00 -0300

    using recycler view. Logic for 3 obligated methods for adapter and holder

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +16,12 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = MainAdapter()
         rv = findViewById(R.id.rv_main)
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(this)
+
+
         //class para admin rv e suas celulas (layout item)
         // Adapter ->
 //        btnImc = findViewById(R.id.btn_imc)
@@ -26,13 +32,17 @@ class MainActivity : AppCompatActivity() {
     }
     // e a classe quye administra o layout
     private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>(){
+        // qual layout utiliza a celula especificada (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
         }
-
+//cantidade de celulas
         override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+            return 15
         }
+        // e disparado toda vez que houver uma rolagem na
+        // tela e seja ncesariao troca a celula
 
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
             TODO("Not yet implemented")
@@ -41,5 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // a clase da celula em si!!
-    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    }
 }
