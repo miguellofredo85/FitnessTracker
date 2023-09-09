3f7b4d17c4cd11ff196b3173197198a088804ecc
Miguel <miguellofredo@gmail.com>
2023-09-09 05:54:27 -0300

    implementing main layout in recycler view, doing the logic for rv and classe for adapter and holder and setting 3 method obligatories for adapter

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,74 +1,77 @@
-    package co.tiagoaguiar.fitnesstracker
+package co.tiagoaguiar.fitnesstracker
 
-    import android.content.Context
-    import android.content.DialogInterface
-    import androidx.appcompat.app.AppCompatActivity
-    import android.os.Bundle
-    import android.util.Log
-    import android.view.View
-    import android.view.View.OnClickListener
-    import android.view.inputmethod.InputMethodManager
-    import android.widget.Button
-    import android.widget.EditText
-    import android.widget.Toast
-    import androidx.annotation.StringRes
-    import androidx.appcompat.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 
-    class ImcActivity : AppCompatActivity() {
-        private lateinit var editHeight: EditText
-        private lateinit var editWeight: EditText
+class ImcActivity : AppCompatActivity() {
+    private lateinit var editHeight: EditText
+    private lateinit var editWeight: EditText
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_imc)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
 
-            editHeight = findViewById(R.id.edit_height)
-            editWeight = findViewById(R.id.edit_weight)
+        editHeight = findViewById(R.id.edit_height)
+        editWeight = findViewById(R.id.edit_weight)
 
-            val btnSend: Button = findViewById(R.id.btn_calcular)
-            btnSend.setOnClickListener {
-                if (!validateForm()) {
-                    Toast.makeText(this, "Input cant be zero or empty", Toast.LENGTH_LONG).show()
-                    return@setOnClickListener // kill event fun
-                }
-                val weight = editWeight.text.toString().toInt()
-                val height = editHeight.text.toString().toInt()
-                val res = calcImc(weight, height)
-                Log.d("Teste", "resultado: $res") // log debug
-
-                val imcResponseId = imcResponse(res)
-                    AlertDialog.Builder(this)
-                    .setTitle(getString(R.string.imc_response, res))
-                    .setMessage(imcResponseId)
-                    .setPositiveButton(android.R.string.ok) { dialog, which -> }
-                    .create()
-                    .show()
-                val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        val btnSend: Button = findViewById(R.id.btn_calcular)
+        btnSend.setOnClickListener {
+            if (!validateForm()) {
+                Toast.makeText(this, "Input cant be zero or empty", Toast.LENGTH_LONG).show()
+                return@setOnClickListener // kill event fun
             }
-        }
-        private fun imcResponse(imc:Double):String{
-          return  when{
-              imc<15.0 -> return "1"
-              imc<16.0 -> return "2"
-              imc<18.5 -> return "3"
-              imc<25.0 -> return "4"
-              imc<30.0 -> return "5"
-              imc<35.0 -> return "6"
-              imc<40.0 -> return "7"
-              else -> "0"
-            }
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+            val res = calcImc(weight, height)
+            Log.d("Teste", "resultado: $res") // log debug
 
-        }
-        private fun calcImc(weight: Int, height: Int):Double{
-            return weight/((height/100.0)*(height/100.0))
-        }
-        private fun validateForm(): Boolean {
-            //not null
-            // not zero
-            return (editWeight.text.toString().isNotEmpty()
-                && editHeight.text.toString().isNotEmpty()
-                && !editWeight.text.toString().startsWith("0")
-                        && !editHeight.text.toString().startsWith("0"))
+            val imcResponseId = imcResponse(res)
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.imc_response, res))
+                .setMessage(imcResponseId)
+                .setPositiveButton(android.R.string.ok) { dialog, which -> }
+                .create()
+                .show()
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
+
+    private fun imcResponse(imc: Double): String {
+        return when {
+            imc < 15.0 -> return "uuuffff too low"
+            imc < 16.0 -> return "too low"
+            imc < 18.5 -> return "low"
+            imc < 25.0 -> return "normal"
+            imc < 30.0 -> return "high"
+            imc < 35.0 -> return "too high"
+            imc < 40.0 -> return "uuuuufff too high"
+            else -> "close to death..."
+        }
+
+    }
+
+    private fun calcImc(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
+    }
+
+    private fun validateForm(): Boolean {
+        //not null
+        // not zero
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
+    }
+}
--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -3,20 +3,43 @@ package co.tiagoaguiar.fitnesstracker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-   private lateinit var btnImc:LinearLayout
-
+//   private lateinit var btnImc:LinearLayout
+    private lateinit var rv : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rv = findViewById(R.id.rv_main)
+        //class para admin rv e suas celulas (layout item)
+        // Adapter ->
+//        btnImc = findViewById(R.id.btn_imc)
+//        btnImc.setOnClickListener {
+//            var intentImc = Intent(this, ImcActivity::class.java)
+//            startActivity(intentImc)
+//        }
+    }
+    // e a classe quye administra o layout
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
 
-        btnImc = findViewById(R.id.btn_imc)
-        btnImc.setOnClickListener {
-            var intentImc = Intent(this, ImcActivity::class.java)
-            startActivity(intentImc)
         }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
     }
 
+    // a clase da celula em si!!
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
