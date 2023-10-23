36eea3e583a77710555dcaa20ba95279bc06811c
Miguel <miguellofredo@gmail.com>
2023-10-23 09:36:55 -0300

    working with Room. companion object  and database, standard database script model, padrao

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/model/AppDatabase.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/model/AppDatabase.kt
@@ -1,9 +1,33 @@
 package co.tiagoaguiar.fitnesstracker.model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Calc::class], version = 1) // se p banco de dados muda a versao tambem
+@Database(entities = [Calc::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun calcDao(): CalcDao
+
+    companion object {
+
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context) : AppDatabase {
+            return if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "fitness_tracker"
+                    ).build()
+                }
+                INSTANCE as AppDatabase
+            } else {
+                INSTANCE as AppDatabase
+            }
+        }
+    }
+
 }
